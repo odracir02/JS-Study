1d5ffa1db5551d720d30991b9f7d807ac7540be1
Ricardo Sastre <ricardosastrelopez@gmail.com>
2022-10-26 16:28:27 +0200

    Buttons

--- Electron/Select/functions.js
+++ Electron/Select/functions.js
@@ -6,23 +6,12 @@ for (let i=0; i < opciones.length; i++) {
 }
 palo.innerHTML = cadena;
 
-console.log(palo.value);
-console.log(palo.selectedIndex);
+//Comprobar cual esta seleccionado en el radio button
+let ra=document.getElementById("radio1");
+if(ra.checked)
+console.log(ra.value);
 
-let option = document.createElement("option");
-option.setAttribute("value", "otro palo más");
-option.text = "otro palo más";
-palo.add(option);
-
-palo.remove(palo.lenght-1);
-
-let imgs = document.getElementsByTagName('img');
-
-let plantilla = `declarando string
-    de más de una línea`
-
-let message = `Número de aciertos:  ${aciertos} 
-    Número de fallos: ${fallos}
-    No contestadas: ${c}`
-
-    
+//Comprobar si esta seleccionado en el checkButton
+let c=document.getElementById("c1");
+if(c.checked)
+console.log(c.value);
